418bac97b373a051129fa7bf5e22fbb841464906
Raphael <distopiatunes@gmail.com>
2024-04-19 16:00:29 -0300

    atualizando hello.py

--- hello.py
+++ hello.py
@@ -27,13 +27,15 @@ current_language = os.getenv("LANG", "en_US")[:5]
 msg = "Hello, World!"
 
 if current_language == "pt_BR":
-     msg = "Olá, Mundo!"
+    msg = "Olá, Mundo!"
 elif current_language == "it_IT":
     msg = "Ciao, Mondo"
 elif current_language == "es_SP":
     msg = "Hola, Mundo"
 elif current_language == "fr_FR":
-    msg = "Bonjour, Monde"    
+    msg = "Bonjour, Monde" 
+elif current_language == "fr_FR":
+           msg = "Bonjour, Monde"    
 
 print(msg)
 
